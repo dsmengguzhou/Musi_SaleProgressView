a20d63f7666da6b927195e2387cd80343414b10a
Musixiaoge <dsmengguzhou@163.com>
2017-09-26 16:07:31 +0800

    just test

--- app/src/main/java/com/musixiaoge/administrator/musi_saleprogressview/MainActivity.java
+++ app/src/main/java/com/musixiaoge/administrator/musi_saleprogressview/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    //抢购进度条
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
